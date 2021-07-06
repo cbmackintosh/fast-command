48c80ee8802a7106dd17f75f196d27931319afc8
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-06 17:12:53 -0600

    Reconfigure dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,35 +1,26 @@
-import React from 'react';
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Home from '../Home/Home' 
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logoutUserThunk } from '../App/AppSlice'
 
-const Dashboard = (props) => {
+const Dashboard = () => {
   
-  const handleClick = () => {
-    axios.delete('http://localhost:3005/logout', {withCredentials: true})
-    .then(response => {
-      props.handleLogout()
-      props.history.push('/')
-    })
-    .catch(error => console.log(error))
+  const dispatch = useDispatch()
+
+  const user = useSelector(state => state.user.user)
+
+  const handleLogout = () => {
+    dispatch(logoutUserThunk())
   }
+  
+  return (
+    <div>
+      <h1>Welcome {user.firstname} {user.lastname}</h1>
+      <p>{user.jobtitle} - {user.organization}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Email: {user.email}</p>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  )
+}
 
-  if (props.loggedInStatus) {
-    return (
-      <div>
-        <h1>Welcome {props.user.firstname} {props.user.lastname}</h1>
-        <p>{props.user.jobtitle} - {props.user.organization}</p>
-        <p>Phone: {props.user.phone}</p>
-        <p>Email: {props.user.email}</p>
-        <Link to='/logout' onClick={handleClick}>Log Out</Link>
-      </div>     
-    )
-  } else {
-    return (
-      <Home {...props} handleLogin={props.handleLogin} loggedInStatus={false}/>
-    )
-  }
-
-};
-
-export default Dashboard;
+export default Dashboard
