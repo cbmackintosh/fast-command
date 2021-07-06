30115be90f1809a18110fa6ab9eb8fb46a5b7feb
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-06 17:05:48 -0600

    Add separate file for api calls

---
+++ src/api-calls.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const baseURL = `http://localhost:3005`
+
+export const checkLoginStatus = () => {
+  return axios.get(`${baseURL}/logged_in`, {withCredentials: true})
+  .then(response => {
+    return response.data
+  })
+}
+
+export const logoutUser = () => {
+  return axios.delete(`${baseURL}/logout`, {withCredentials: true})
+  .then(response => {
+    return response.data
+  })
+}
+
+export const loginUser = (user) => {
+  return axios.post(`${baseURL}/login`, {user}, {withCredentials: true})
+  .then(response => {
+    return response.data
+  })
+  .catch(error => console.log('api errors:', error))
+}
+
+export const signupUser = (user) => {
+  return axios.post(`${baseURL}/users`, {user}, {withCredentials: true})
+  .then(response => {
+    return response.data
+  })
+  .catch(error => console.log('api errors:', error))
+}
