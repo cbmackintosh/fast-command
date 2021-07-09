61a10fab34b14ed2a043343eb12342b3ee7082ce
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-09 12:08:34 -0600

    Establish conditional rendering for hamburger icon depending on viewport width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import './Navbar.css'
 
 export const Navbar = () => {
 
+  const [screenWidth, setScreenWidth] = useState(0)
+
+  useEffect(() => {
+    setScreenWidth(visualViewport.width)
+    window.addEventListener("resize", () => {
+      setScreenWidth(visualViewport.width)
+    })
+  }, [])
 
   return (
     <div className='navbar'>
-      <GiHamburgerMenu />
+      {screenWidth < 600 && <GiHamburgerMenu />}
       <Link>Home</Link>
       <Link>Contacts</Link>
       <Link>New Incident</Link>
