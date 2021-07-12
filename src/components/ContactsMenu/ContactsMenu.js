43819a60d169af83d33c420149243e39adf6fdfc
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-12 14:51:54 -0600

    Add styling to ContactsMenu

--- src/components/ContactsMenu/ContactsMenu.js
+++ src/components/ContactsMenu/ContactsMenu.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Navbar } from '../Navbar/Navbar'
 import NewContactForm from '../NewContactForm/NewContactForm'
+import './ContactsMenu.css'
 
 const ContactsMenu = () => {
   return (
-    <div>
+    <div className='contacts-menu'>
       <Navbar />
       <NewContactForm />
     </div>
