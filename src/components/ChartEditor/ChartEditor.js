0221e236656ede56e35835d8af1ebeab29f9dfb1
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-19 10:52:56 -0600

    Experiment with chart node templates and conditional button rendering

--- src/components/ChartEditor/ChartEditor.js
+++ src/components/ChartEditor/ChartEditor.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { OrgDiagram } from 'basicprimitivesreact'
 import { PageFitMode, GroupByType, Enabled, ItemType, AdviserPlacementType, ChildrenPlacementType, Colors } from 'basicprimitives';
 import './ChartEditor.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { BiUserCircle } from 'react-icons/bi'
+
+
 
 class ChartEditor extends Component {
   render() {
@@ -11,13 +15,44 @@ class ChartEditor extends Component {
       highlightItem: 0,
       arrowsDirection: GroupByType.Children,
       hasSelectorCheckbox: Enabled.False,
+      hasButtons: Enabled.True,
+      buttonsPanelSize: 40,
+      onButtonsRender: (({ context: itemConfig }) => {
+        if (itemConfig.id > 5) {
+          return <>
+            <button onClick={() => console.log('button clicked!')}>
+              <GiHamburgerMenu />
+            </button>
+          </>
+        }
+      }),
+      templates: [{
+        name: 'contactTemplate',
+        itemSize: { width: 220, height: 120 },
+        onItemRender: ({ context: itemConfig }) => {
+          return <div className="ContactTemplate">
+            <div className="ContactTitleBackground">
+              <div className="ContactTitle">{itemConfig.title}</div>
+            </div>
+            <div className="ContactPhotoFrame">
+              <BiUserCircle />
+            </div>
+            <div className="ContactDescription">{itemConfig.description}</div>
+            <div className="ContactPhone">Phone: {itemConfig.phone}</div>
+            <div className="ContactEmail">Email: {itemConfig.email}</div>
+          </div>; 
+        }
+      }],
       items: [
         {
           id: 0,
           parent: null,
           title: 'Incident Commander',
           description: 'Unassigned',
-          image: '/api/images/photos/a.png'
+          image: '/api/images/photos/a.png',
+          email: 'test@test.org',
+          phone: '',
+          templateName: 'contactTemplate'
         },
         {
           id: 1,
@@ -52,7 +87,8 @@ class ChartEditor extends Component {
           description: 'Unassigned',
           image: '/api/images/photos/c.png',
           itemType: ItemType.Assistant,
-          adviserPlacementType: AdviserPlacementType.Right
+          adviserPlacementType: AdviserPlacementType.Right,
+          hasButtons: Enabled.True
         },
         {
           id: 5,
