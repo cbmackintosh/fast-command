d49521a2311bed27f2d7277543160a53ee9ba838
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-07-28 14:22:48 -0600

    Add back button on IncidentManager

--- src/components/IncidentManager/IncidentManager.js
+++ src/components/IncidentManager/IncidentManager.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import { getIncidentBySlug } from '../../api-calls'
 import ChartEditor from '../ChartEditor/ChartEditor'
 
@@ -16,6 +17,8 @@ const IncidentManager = ({ slug }) => {
       <h1>{incident.name}</h1>
       <h4>{incident.incident_type} | {incident.location} | ACTIVE SINCE: {incident.created_at}</h4>
       <p>{incident.summary}</p>
+      <Link to='/dashboard'><button>BACK</button></Link>
+      <button>RESOLVE</button>
       {incident.id && <ChartEditor incident_id={incident.id} />}
     </div>
   )
