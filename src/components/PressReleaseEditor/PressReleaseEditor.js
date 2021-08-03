fe22bfb42d524905df36643fa884cc3322b5586c
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-08-03 09:17:19 -0600

    Add post submission functionality to form

--- src/components/PressReleaseEditor/PressReleaseEditor.js
+++ src/components/PressReleaseEditor/PressReleaseEditor.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { postPressRelease } from '../../api-calls'
 
-const PressReleaseEditor = () => {
+const PressReleaseEditor = (props) => {
+
+  console.log(props)
+  const [headline, setHeadline] = useState('')
+  const [body, setBody] = useState('')
+
+  const postNewPressRelease = e => {
+    e.preventDefault()
+    postPressRelease(props.incident_id, headline, body)
+    .then(response => console.log(response))
+  }
 
   return (
-    <form>
+    <form onSubmit={e => postNewPressRelease(e)}>
 
       <label htmlFor='headline'>Headline</label>
       <input
         name='headline'
+        onChange={e => setHeadline(e.target.value)}
+        value={headline}
       >
       </input>
 
       <label htmlFor="post_body">Body:</label>
       <textarea
         name="post_body"
+        onChange={e => setBody(e.target.value)}
+        value={body}
       />
 
-      <button>SUBMIT</button>
+      <button type='submit'>SUBMIT</button>
 
     </form>
   ) 
