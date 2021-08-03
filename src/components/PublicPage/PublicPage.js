20896e568284459d34df676c4189f84d83e3c407
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-08-03 11:20:42 -0600

    Display press releases on public facing page

--- src/components/PublicPage/PublicPage.js
+++ src/components/PublicPage/PublicPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { getIncidentBySlug } from '../../api-calls'
 import PublicChart from '../PublicChart/PublicChart'
+import PressReleaseList from '../PressReleaseList/PressReleaseList'
+import { Link } from 'react-router-dom'
 
 const IncidentManager = ({ slug }) => {
   
@@ -22,11 +24,13 @@ const IncidentManager = ({ slug }) => {
   
   return (
     <div>
+      <Link to='/'><button>LOGIN</button></Link>
       <h1>{incident.name}</h1>
       <h4>{incident.incident_type} | {incident.location} | ACTIVE SINCE: {incident.created_at}</h4>
       <p>{incident.summary}</p>
       <button onClick={toggleView}>{view === 'chart' ? 'Press Releases' : 'Incident Command Chart'}</button>
       {incident.id && view === 'chart' ? <PublicChart incident_id={incident.id} /> : null}
+      {incident.id && view === 'posts' ? <PressReleaseList incident_id={incident.id} /> : null}
     </div>
   )
 }
